d89545051bf5a8c1922d147d3cb84ee4840adfa1
pretty3831 <46170310+pretty3831@users.noreply.github.com>
2019-01-23 17:47:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -378,29 +378,4 @@ client.user.setStatus("dnd")
 }); 
 
 
-client.on('message', message => {
-  if (!message.content.startsWith(PREFIX)) return;
-  var args = message.content.split(' ').slice(1);
-  var argresult = args.join(' ');
-  if (message.author.id !== "433198762706599936") return;
-
-
-
-if (message.content.startsWith(PREFIX + 'setstream')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/darkknite55");
-	 console.log('test' + argresult);
-    message.channel.sendMessage(`Streaming: **${argresult}`)
-} 
-
-if (message.content.startsWith(PREFIX + 'setname')) {
-  client.user.setUsername(argresult).then
-	  message.channel.sendMessage(`Username Changed To **${argresult}**`)
-  return message.reply("You Can change the username 2 times per hour");
-} 
-if (message.content.startsWith(PREFIX + 'setavatar')) {
-  client.user.setAvatar(argresult);
-   message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
-}
-});
-
 client.login("NTI0Nzg4NTkwNjY5MjAxNDE4.DyocNA.k2EqbS2s5aXWtzqmoxDYWix_WpE");
